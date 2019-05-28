bab28f02bd9f94c5b0232eb547abd3ac9c08ffc7
eugenereydel99 <eugenereydel@gmail.com>
2019-05-28 22:49:30 +0700

    Убывающая последовательность

---
+++ 737-2_rev-practice2-2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n;
+	scanf("%d",&n);
+	
+	int x,count=0,temp=0;
+	
+	for (int i=0;i<n;i++){
+		scanf("%d",&x);
+		if (i==0){temp=x;continue;}
+		
+		if	(temp>=x){
+			count+=1;
+		}else if (temp<x){
+			count-=1;
+		}
+		temp=x;
+	}
+	
+	if (count==n-1){
+		printf("%d",1);
+	}else{
+		printf("%d",0);
+	}
+	
+	return 0;
+}
